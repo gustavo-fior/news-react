3ae4edf90895f1092bc325d4f298a62883677ae3
Gustavo Fior <gustavo_fior@outlook.com>
2022-02-01 23:48:26 -0300

    Idealizando index

--- src/components/FormularioInicial/FormularioInicial.jsx
+++ src/components/FormularioInicial/FormularioInicial.jsx
@@ -1,14 +1,33 @@
-import { Container, TextField, Typography } from "@mui/material";
+import { Container, TextField, Typography, Box, Button } from "@mui/material";
 import React from "react";
 
 function FormularioIncial() {
   return (
-    <Container maxWidth="sm">
-      <Typography maxWidth variant="h3" component="h1">
-        Brazil News
-      </Typography>
-      <TextField maxWidth variant="standard" placeholder="Digite a palavra que deseja buscar."/>
-    </Container>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Container maxWidth="md">
+        <Typography align="center" maxWidth variant="h3" component="h1">
+          Brazil News
+        </Typography>
+        <form>
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="standard"
+            placeholder="Digite a palavra que deseja buscar"
+          />
+          <Box margin={5} textAlign="center">
+            <Button type="submit"  variant="outlined">
+              Buscar
+            </Button>
+          </Box>
+        </form>
+      </Container>
+    </Box>
   );
 }
 
