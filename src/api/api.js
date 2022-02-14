084961870fb5ec281a21a9c97ed721c9373734ba
Gustavo Fior <72475351+gustavo-fior@users.noreply.github.com>
2022-02-14 17:58:51 -0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api =
     axios.create({
-        baseURL: "http://localhost:8080"
+        baseURL: "https://brazil-news.herokuapp.com"
     });
 
 export const auth = async (email, senha) => {
