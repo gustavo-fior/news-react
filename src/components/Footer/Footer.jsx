99e36f469600c9c2d85d6e0ce94aa9e680facd18
Gustavo Fior <72475351+gustavo-fior@users.noreply.github.com>
2022-02-14 13:18:44 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,13 +7,13 @@ const Footer = () => {
         <Box display="flex" alignItems="center"
         justifyContent="center" className='margem-cima'>
             <footer className='footer'>
-                <Link className='footer-link1' href='https://github.com'>
+                <Link className='footer-link1' href='https://github.com/gustavo-fior/news-react'>
                     <p className='footer-texto'>GitHub</p>
                 </Link>
-                <Link className='footer-link2' href='https://github.com'>
+                <Link className='footer-link2' href='https://rapidapi.com/gustavo_fior@outlook.com/api/brazil-news/'>
                     <p className='footer-texto'>API</p>
                 </Link>
-                <Link className='footer-link3' href='https://github.com'>
+                <Link className='footer-link3' href='https://github.com/gustavo-fior'>
                     <p className='footer-texto'>Developer</p>
                 </Link>
             </footer>
