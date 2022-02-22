538d64af4ee7c724c60230982b676e8ee0c1dec2
Gustavo Fior <72475351+gustavo-fior@users.noreply.github.com>
2022-02-22 12:35:53 -0300

    Update Formulario.jsx

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -42,7 +42,7 @@ function Formulario() {
       <form
         onSubmit={(event) => {
           event.preventDefault();
-          buscaNoticias(palavra.toLocaleLowerCase, setNoticias, jornal);
+          buscaNoticias(palavra.toLocaleLowerCase(), setNoticias, jornal);
         }}
       >
         <Grid container spacing={2}>
