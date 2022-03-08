02da3ec97d465e79a462c52f04c2763097345311
Gustavo Fior <gustavo_fior@outlook.com>
2022-03-08 10:25:57 -0300

    Modificando reqs para api

--- src/api/api.js
+++ src/api/api.js
@@ -1,61 +1,61 @@
 import axios from "axios";
 
 const getTokenFromSessionStorage = () => {
-  return sessionStorage.getItem("token");
+    return sessionStorage.getItem("token");
 };
 
 const header = {
-  headers: {
-    Authorization: "Bearer " + getTokenFromSessionStorage(),
-  },
+    headers: {
+        Authorization: "Bearer " + getTokenFromSessionStorage(),
+    },
 }
 
 export const api = axios.create({
-  baseURL: "https://brazil-news.herokuapp.com",
+    baseURL: "https://brazil-news.herokuapp.com",
 });
 
-export const auth = async (email, senha) => {
-  const body = {
-    email: email,
-    senha: senha,
-  };
+export const auth = async(email, senha) => {
+    const body = {
+        email: email,
+        senha: senha,
+    };
 
-  await api
-    .post("/auth", body)
-    .then((res) => {
-      sessionStorage.setItem("token", res.data.token);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    await api
+        .post("/auth", body)
+        .then((res) => {
+            sessionStorage.setItem("token", res.data.token);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 };
 
-export const buscaNoticias = async (palavra, setDado, jornal) => {
-  let res = [];
-  if (jornal === "none") {
-    res = await api.get(`/news/${palavra}`, header);
-  } else {
-    res = await api.get(`/news/${jornal}/${palavra}`, header);
-  }
+export const buscaNoticias = async(palavra, setDado, jornal) => {
+    let res = [];
+    if (jornal === "none") {
+        res = await api.get(`/news/${palavra}`, header);
+    } else {
+        res = await api.get(`/news/${jornal}/${palavra}`, header);
+    }
 
-  setDado(res.data);
+    setDado(res.data);
 };
 
-export const buscaJornais = async (setDado) => {
-  const res = await api.get(`/newspaper`);
-  setDado(res.data);
+export const buscaJornais = async(setDado) => {
+    const res = await api.get(`/newspaper`);
+    setDado(res.data);
 };
 
-export const buscaTrends = async (setDado, trendLink, setDado2) => {
+export const buscaTrends = async(setDado, trendLink, setDado2) => {
 
-  let res = [];
+    let res = [];
 
-  if (trendLink) {
-    res = await api.get(`/trends/${trendLink}`, header);
-    setDado2(res.data.noticias);
-  } else {
-    res = await api.get(`/trends`, header);
-  }
+    if (trendLink) {
+        res = await api.get(`/trends/${trendLink}`, header);
+        setDado2(res.data.noticias);
+    } else {
+        res = await api.get(`/trends`, header);
+    }
 
-  setDado(res.data);
+    setDado(res.data);
 };
