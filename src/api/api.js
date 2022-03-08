b416360dfc6ce7d81ad6c6b990a838d2b7077ef3
Gustavo Fior <gustavo_fior@outlook.com>
2022-03-08 17:12:48 -0300

    Alterando home

--- src/api/api.js
+++ src/api/api.js
@@ -11,7 +11,7 @@ const header = {
 }
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://brazil-news.herokuapp.com",
 });
 
 export const auth = async(email, senha) => {
